66c0004d1adc8121735045e453e81314ec7f9c1c
Michael Davids <michaelcdavids@gmail.com>
2018-11-09 08:19:49 +0200

    working on functionality

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-'use strict';
 const express = require('express');
-const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-const session = require('express-session');
+const exphbs = require('express-handlebars');
 const flash = require('express-flash');
-const FactoryFunction = require('./services/strava-app-factory');
-const APIroutes = require('./routes/strava-app-routes');
-const app = express();
 const pg = require("pg");
+const FactoryFunction = require('./services/factory');
+const Routes = require('./routes/routes');
+
+const session = require('express-session');
+
+
+
 const Pool = pg.Pool;
 let useSSL = false;
 let local = process.env.LOCAL || false;
@@ -19,13 +21,21 @@ const pool = new Pool({
     connectionString,
     ssl: useSSL
 });
+
+
+
 const Instance = FactoryFunction(pool);
-const apiRoutes = APIroutes(Instance);
+const appRoutes = Routes(Instance);
+
+const app = express();
+
 app.use(session({
     secret: 'just do it',
     resave: false,
     saveUninitialized: true
 }));
+
+// Middlewares
 app.use(flash());
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
@@ -45,10 +55,17 @@ function errorHandler(err, req, res, next) {
 }
 app.use(errorHandler);
 
-app.get('/', apiRoutes.indexGet);
-app.post("/", apiRoutes.indexPost);
-app.get('/profile', apiRoutes.profile);
-app.get('/list_activities', apiRoutes.listActivities);
+// Create a mobile application or web site that will:
+// Only deal with cycling and running activities
+
+
+// Routes
+app.get('/', appRoutes.indexGet); 
+app.post("/", appRoutes.indexPost);  // Connect to the users Strava profile via the Strava API and retrieve the activity history for last 3 months or since inception if less
+app.get('/profile', appRoutes.profile); 
+
+
+app.get('/list_activities', appRoutes.listActivities);
 
 
 
@@ -56,6 +73,7 @@ app.get('/list_activities', apiRoutes.listActivities);
 
 const PORT = process.env.PORT || 3011;
 app.listen(PORT, function () {
-    console.log('Strava App started and running listening on port:', PORT);
+    console.log('App started and listening on port:', PORT);
 });
-module.exports = app;
+
+// module.exports = app;
---
+++ services/factory.js
@@ -0,0 +1,51 @@
+module.exports = function (pool) {
+
+    async function cyclingSummary() {
+        // For cycling, display:
+        //     Average number of rides per week
+        // Average distance per ride
+        // Average speed per ride
+        // Fastest average speed
+        // for a ride
+        // Longest ride distance
+        // Longest ride duration
+    }
+    async function runSummary() {
+        // For running, display:
+        //     Average number of runs per week
+        // Average distance per run
+        // Average pace per run
+        // Fastest average pace
+        // for a run
+        // Longest run distance
+        // Longest run duration
+    }
+    async function saveSummary() {
+        // Allow user to save summary with effective date
+    }
+    async function getSummary() {
+        //When user logs in again, retrieve and display summary history
+    }
+
+    // Github Repo & Link
+    // Go to necessary depth for the app
+    // API
+    // Database
+    // Add any bells and whistles that you think will add value, showing what you, as a developer, are capable of doing.
+
+    // holistic approach
+    // software engineer
+    // approach to problems
+    // problem solving
+    // development methodology
+    // result
+    // Use any languages, frameworks, etc. that you prefer.
+
+    // Any assumptions you make, note them in the code or in a response email
+    return {
+        runSummary,
+        cyclingSummary,
+        saveSummary,
+        getSummary
+    }
+}
